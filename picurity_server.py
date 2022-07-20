7f7e35269f8ed38158c8c045f6791b28dc1b85d6
obiPi <obi.serra@gmail.com>
2022-07-20 09:09:25 +0100

    first commit

---
+++ picurity_server.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+from flask import Flask
+
+
+
+
+
+
+
+
+app = Flask(__name__)
+
+@app.route("/")         # This is our default handler, if no path is given
+def index():
+    return "hello"
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080)
