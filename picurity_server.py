b5a9f98771caca739c7b4a31db42b43af12d62e7
Roberto Serra <roberto.serra@etiqa.it>
2022-07-22 23:10:35 +0200

    pipfile

--- picurity_server.py
+++ picurity_server.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 
-
 from flask import Flask
-
-
-
-
-
-
 
 
 app = Flask(__name__)
 
+
 @app.route("/")         # This is our default handler, if no path is given
 def index():
     return "hello"
-
 
 
 if __name__ == "__main__":
