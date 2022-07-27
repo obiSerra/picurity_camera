98b980dbc8f07578c787261b7905be1b727f6ec7
obi@picurity <obi.serra@gmail.com>
2022-07-27 17:28:17 +0100

    stream created

--- picurity_camera/stream.py
+++ picurity_camera/stream.py
@@ -1,27 +1,32 @@
 import cv2
 
-#from picamera2 import Picamera2
+from picamera2 import Picamera2
+import libcamera
 
-cam = cv2.VideoCapture(0)
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+#cam = cv2.VideoCapture(0)
+#cam.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+#cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_preview_configuration(
-    main={"format": 'XRGB8888', "size": (640, 480)}))
+capture_config = picam2.create_still_configuration(
+    main={"format": 'XRGB8888',
+          "size": (640, 480)
+})
+capture_config["transform"] = libcamera.Transform(hflip=1, vflip=1)
+
+picam2.configure(capture_config)
 picam2.start()
 
 
 def capture_video_old():
-
-    _, img = cam.read()
-    _, frame = cv2.imencode('.jpg', img)
-    return frame
+    pass
+ #   _, img = cam.read()
+ #   _, frame = cv2.imencode('.jpg', img)
+  #  return frame
 
 
 def capture_video():
-
-    _, img = picam2.read()
+    img = picam2.capture_array()
     _, frame = cv2.imencode('.jpg', img)
     return frame
